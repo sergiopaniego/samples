4f846cd9e4bb3995fe7327829f6f9ffcb320e356
sergiopaniego <sergiopaniegoblanco@gmail.com>
2025-04-22 17:23:14 +0200

    update smolagents MCP sample

--- smolagents_mcp.py
+++ smolagents_mcp.py
@@ -1,4 +1,5 @@
-from smolagents import ToolCollection, CodeAgent, HfApiModel
+from smolagents import CodeAgent, HfApiModel
+from smolagents.mcp_client import MCPClient
 from mcp import StdioServerParameters
 import os
 
@@ -11,6 +12,6 @@ server_parameters = StdioServerParameters(
     args=["-y", "@peng-shawn/mermaid-mcp-server"],
 )
 
-with ToolCollection.from_mcp(server_parameters, trust_remote_code=True) as tool_collection:    
-    agent = CodeAgent(tools=[*tool_collection.tools], model=model, add_base_tools=False)
+with MCPClient(server_parameters) as tools:    
+    agent = CodeAgent(tools=tools, model=model, add_base_tools=False)
     agent.run("Can you generate a diagram for a simple flowchart with three nodes: Start, Process, End and save it to a PNG file?")
